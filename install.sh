4b610052738012e75d1a39980f79215ba7f9c40c
Fada Gelada <diegoflorenciomartins@gmail.com>
2025-06-28 20:25:13 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,18 +1,37 @@
 #!/bin/bash
-USER_HOME="$(dirname "$(dirname "$(realpath "$0")")")"
 
 SCRIPT_DIR="$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &> /dev/null && pwd)"
 base="$SCRIPT_DIR/Resources"
 
-sudo pacman -S --needed sddm qt5-graphicaleffects qt5-quickcontrols qt5-quickcontrols2 qt5-svg qt5-declarative qt5-translations noto-fonts ttf-roboto ttf-nerd-fonts-symbols ttf-nerd-fonts-symbols-common ttf-font-awesome
+sudo pacman -S --needed \
+    sddm \
+    qt5-graphicaleffects \
+    qt5-quickcontrols \
+    qt5-quickcontrols2 \
+    qt5-svg \
+    qt5-declarative \
+    qt5-translations \
+    noto-fonts \
+    ttf-roboto \
+    ttf-nerd-fonts-symbols \
+    ttf-nerd-fonts-symbols-common \
+    ttf-font-awesome \
+    kitty \
+    swww
 
 sudo cp -r "$base/Vimix" /usr/share/grub/themes/
 sudo cp -r "$base/sugar-candy" /usr/share/sddm/themes/
 sudo cp "$base/sddm.conf" /etc/sddm.conf
 sudo cp "$base/grub" /etc/default/grub
-sudo cp -r "$base/wofi" "$USER_HOME/.config/wofi"
-sudo cp -r "$base/waybar" "$USER_HOME/.config/waybar"
-sudo cp "$base/hyprland.conf" "$USER_HOME/hypr"
-sudo cp "$base/Backgrounds/eva1.jpg" /usr/share/backgrounds/eva1.jpg
-sudo grub-mkconfig -o /boot/grub/grub.cfg
 
+mkdir -p "$HOME/.config/wofi"
+mkdir -p "$HOME/.config/waybar"
+mkdir -p "$HOME/.config/hypr"
+
+cp -r "$base/wofi/"* "$HOME/.config/wofi/"
+cp -r "$base/waybar/"* "$HOME/.config/waybar/"
+cp "$base/hyprland.conf" "$HOME/.config/hypr/"
+
+sudo cp "$base/Backgrounds/eva1.jpg" /usr/share/backgrounds/eva1.jpg
+
+sudo grub-mkconfig -o /boot/grub/grub.cfg
